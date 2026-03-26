ff33c51b7c5ddc6103625a88baf4d6a8a7a3a325
Sergiy Balobin <sbalu@ukr.net>
2026-03-26 18:45:37 +0200

    Added todos and test projects

---
+++ NetSdrLogger.Model.Tests/UnitTest1.cs
@@ -0,0 +1,42 @@
+namespace NetSdrLogger.Model
+{
+    //todo
+    // 1. DI
+    // 2. MVVM
+    // 3. Tests
+    // 4. ColumnSort
+
+
+    public class CollectionServiceTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+    public class TCPSignalSourceTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+    public class HardwareHeaderTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+    public class SignalTransmittion
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
