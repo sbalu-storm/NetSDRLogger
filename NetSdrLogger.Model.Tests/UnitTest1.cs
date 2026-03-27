e7a91f1e442e866b24cf7aa735bad9fbb88e29d6
Sergiy Balobin <sbalu@ukr.net>
2026-03-27 07:45:45 +0200

    Fixed todos

--- NetSdrLogger.Model.Tests/UnitTest1.cs
+++ NetSdrLogger.Model.Tests/UnitTest1.cs
@@ -1,11 +1,29 @@
 namespace NetSdrLogger.Model
 {
-    //todo
-    // 1. DI
-    // 2. MVVM
-    // 3. Tests
-    // 4. ColumnSort
+    //byte sequence correctly translated to header and dataitem
+    //dataitem correctly translated to a single_collection
 
+    //two dataitems correctly translated to a single_collection
+    //two dataitems correctly translated to two collections
+    //three dataitems correctly translated to two collections
+
+    //collection aggregation works fine: 
+    //in frequence scope, out of frequency scope
+
+    // average frequency, other avg calculated correctly
+
+    // average frequency handled correctly: averagefreq is the bounds to agregate new signal or not
+
+    //Unix?DateTime is handled correctly
+
+    //TCP socket mocked stream is handled correctly
+    // for one message
+    // for several messages : 
+    //      two good messages
+    //      bad,two good,bad,good messages
+    //      good with error message
+    // for one data item
+    // for several data items
 
     public class CollectionServiceTests
     {
